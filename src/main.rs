228f82890e72bbc8682cd28d4567c5cec3cc84fd
jrgold <jrgold@users.noreply.github.com>
2023-12-01 17:10:18 +1100

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,13 @@ fn main() {
         _ => unreachable!(),
     };
 
+    let input_filename = format!("input/day_{:02}.txt", day_number);
+    // let input_filename = "day_01_sample.txt";
+
+    let input = std::fs::read_to_string(&input_filename).unwrap();
+
     let input_processing_start = Instant::now();
-    let solution = day.process_input("1\n2\n");
+    let solution = day.process_input(&input);
     let input_processing_time = input_processing_start.elapsed();
     println!("Input processing time: {}ms", input_processing_time.as_millis());
 
@@ -42,16 +47,19 @@ fn main() {
 
 mod day_01 {
     use std::fmt::Display;
+    use regex::Regex;
     use crate::day::{Day, Solution};
 
     pub struct Day01;
     struct Input {
-        input: Vec<i32>
+        input: Vec<String>
     }
 
     impl Day for Day01 {
         fn process_input(&self, input: &str) -> Box<dyn Solution> {
-            let input = vec![];
+            let input = input.lines()
+                .map(|line| line.to_owned())
+                .collect();
 
             Box::new(Input {
                 input
@@ -61,15 +69,46 @@ mod day_01 {
 
     impl Solution for Input {
         fn part_1(&self) -> Box<dyn Display> {
-            let answer = "";
+            let answer = self.input.iter()
+                .map(|line| {
+                    let first = line.chars().filter(|c| c.is_numeric()).nth(0).unwrap();
+                    let last = line.chars().rev().filter(|c| c.is_numeric()).nth(0).unwrap();
+                    (first as i32 - '0' as i32) * 10 + (last as i32 - '0' as i32)
+                }).sum::<i32>();
 
             Box::new(answer)
         }
 
         fn part_2(&self) -> Box<dyn Display> {
-            let answer = "";
+            let forward_pattern = Regex::new("^.*?([0-9]|one|two|three|four|five|six|seven|eight|nine)").unwrap();
+            let backward_pattern = Regex::new("^.*?([0-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)").unwrap();
+
+            let answer = self.input.iter().map(|line| {
+                let first = forward_pattern.captures(line).unwrap().get(1).unwrap().as_str();
+                let reversed = &line.chars().rev().collect::<String>();
+                let last = backward_pattern.captures(reversed).unwrap().get(1).unwrap().as_str();
+                to_num(first) * 10 + to_num(&last.chars().rev().collect::<String>())
+            })
+            .sum::<i32>();
 
             Box::new(answer)
+        }
+    }
+
+    fn to_num(s: &str) -> i32 {
+        match s {
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            _ => {
+                s.parse().unwrap()
+            },
         }
     }
 }
