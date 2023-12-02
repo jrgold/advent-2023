c4f75355e3f860b92d5597ad6e86c70f504001d3
jrgold <jrgold@users.noreply.github.com>
2023-12-02 16:52:40 +1100

    Day 2

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,16 @@ mod day {
 }
 
 fn main() {
-    let day_number = 1;
+    let day_number = 2;
 
     let day: &dyn Day = match day_number {
         1 => &day_01::Day01{},
+        2 => &day_02::Day02{},
         _ => unreachable!(),
     };
 
     let input_filename = format!("input/day_{:02}.txt", day_number);
-    // let input_filename = "day_01_sample.txt";
+    // let input_filename = "input/day_02_sample.txt";
 
     let input = std::fs::read_to_string(&input_filename).unwrap();
 
@@ -109,6 +110,88 @@ mod day_01 {
             _ => {
                 s.parse().unwrap()
             },
+        }
+    }
+}
+
+mod day_02 {
+    use std::collections::HashMap;
+    use std::fmt::Display;
+    use std::str::FromStr;
+    use crate::day::{Day, Solution};
+
+    #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+    enum Colour { R, B, G }
+
+    impl FromStr for Colour {
+        type Err = ();
+
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            match s {
+                "red" => Ok(Self::R),
+                "blue" => Ok(Self::B),
+                "green" => Ok(Self::G),
+                _ => Err(()),
+            }
+        }
+    }
+
+    pub struct Day02;
+    struct Input {
+        input: Vec<(i32, Vec<Vec<(Colour, i32)>>)>
+    }
+
+    impl Day for crate::day_02::Day02 {
+        fn process_input(&self, input: &str) -> Box<dyn Solution> {
+            let input = input.lines()
+                .map(|line| {
+                    let (name, rest) = line.split_once(": ").unwrap();
+                    let games = rest.split("; ")
+                        .map(|g|
+                            g.split(", ").map(|b| {
+                                let (n, c) = b.split_once(" ").unwrap();
+                                (c.parse().unwrap(), n.parse().unwrap())
+                            }).collect()
+                        ).collect();
+                    (name.split_once(" ").unwrap().1.parse().unwrap(), games)
+                }).collect();
+
+            Box::new(Input {
+                input
+            })
+        }
+    }
+
+    impl Solution for Input {
+        fn part_1(&self) -> Box<dyn Display> {
+            let limits: HashMap::<Colour, i32> = HashMap::from([(Colour::R, 12), (Colour::G, 13), (Colour::B, 14)]);
+            let answer: i32 = self.input.iter()
+                .filter_map(|(i, rounds)| {
+                    let illegal = rounds.iter()
+                        .flat_map(|r| r.iter())
+                        .any(|(col, n)| n > limits.get(col).unwrap());
+                    if !illegal { Some(i) } else { None }
+                })
+                .sum();
+
+            Box::new(answer)
+        }
+
+        fn part_2(&self) -> Box<dyn Display> {
+            let answer: i32 = self.input.iter()
+                .map(|(_, rounds)| {
+                    let mut mins = HashMap::new();
+                    rounds.iter()
+                        .flat_map(|r| r.iter())
+                        .for_each(|(col, n)| {
+                            mins.entry(*col)
+                                .and_modify(|old: &mut i32| *old = (*old).max(*n))
+                                .or_insert(*n);
+                        });
+                    mins.values().product::<i32>()
+                }).sum();
+
+            Box::new(answer)
         }
     }
 }
